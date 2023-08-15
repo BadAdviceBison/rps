681dfe6aed1ec121f09af4b6dcc0f76e6fdc2d09
BadAdviceBison <139302668+BadAdviceBison@users.noreply.github.com>
2023-08-15 07:41:54 -0700

    Add counters, status change, endgame check

--- rpsJS.js
+++ rpsJS.js
@@ -1,66 +1,56 @@
-/*
-Steps:
-Get bot choice
-get human choice
-compare choices
-announce winner
-
-/* 
-Rock = 1
-Paper = 2
-Scissors = 3
-*/
 let playerScore = 0;
 let botScore = 0;
 let playerSelection = 0;
 
+updateCounter()
+
 //Make button background blue on click for player select, red for bot reveal, purple for bot reveal tie, maybe add small delays for effect.
-
-const rock = document.querySelector('#rock')
-rock.addEventListener('click', () => playerSelection = 1);
-
-const paper = document.querySelector('#paper')
-paper.addEventListener('click', () => playerSelection = 2);
-
-const scissors = document.querySelector('#scissors')
-scissors.addEventListener('click', () => playerSelection = 3);
 
 function getComputerChoice(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
     }
 
-let botPick = getComputerChoice(1,4)
+let botPick = 0
 
-function pConvert(newVal2) {
-    if (newVal2 == 'r') {
-        playerSelection = 1
-    } else if (newVal2 == 'p') {
-        playerSelection = 2
-    } else if (newVal2 == 's') {
-        playerSelection = 3
-    } else {
-        playerSelection = 100
-    }
+const rock = document.querySelector('#rock')
+rock.addEventListener('click', () => {
+    playerSelection = 1
+    botPick = getComputerChoice(1,4)
+    compare(playerSelection, botPick)
+    scoreCheck()
+    })
+
+const paper = document.querySelector('#paper')
+paper.addEventListener('click', () => {
+    playerSelection = 2
+    botPick = getComputerChoice(1,4)
+    compare(playerSelection, botPick)
+    scoreCheck()
+})
+
+const scissors = document.querySelector('#scissors')
+scissors.addEventListener('click', () => {
+    playerSelection = 3
+    botPick = getComputerChoice(1,4)
+    compare(playerSelection, botPick)
+    scoreCheck()
+})
+
+const buttons = document.querySelectorAll('.rpsButton')
+buttons.forEach(button => button.addEventListener('click', updateCounter))
+
+function updateCounter() {
+    document.getElementById('counterPlayer').innerText = `Player: ${playerScore}`
+    document.getElementById('counterBot').innerText = `Bot: ${botScore}`
 }
 
-pConvert(playerSelection)
-
-function confirmSelection() { 
-    if (playerSelection == 1) {
-        alert('You picked rock.');
-    } 
-    else if (playerSelection == 2) {
-        alert('You picked paper.');
-    } 
-    else if (playerSelection == 3) {
-        alert('You picked scissors.');
-    } 
-    else {
-        alert('That\'s not a valid selection');
+function scoreCheck() {
+    if (playerScore === 5) {
+        document.getElementById('status').innerText = 'Congratulations! \n\n You\'ve saved yourself from the clutches of a TechnoTyrant!'
+    } else if (botScore === 5) {
+        document.getElementById('status').innerText = 'Oh no! You\'ve been condemned to a life of servitude! Unless...'
     }
 }
-
-confirmSelection()
 
 /* 
 Rock = 1
@@ -74,30 +64,32 @@ function compare(player, bot) {
     } 
     else if (player == 2 && bot == 1) {
         alert('The bot chose rock. You win! :D')
+        playerScore += 1
     } 
     else if (player == 3 && bot == 1) {
         alert('The bot chose rock. You lose :(')
+        botScore += 1
     }
     else if (player == 1 && bot == 2) {
         alert('The bot chose paper. You lose :(')
+        botScore += 1
     }
     else if (player == 2 && bot == 2) {
         alert('The bot also chose paper. It\'s a draw!')
     }
     else if (player == 3 && bot == 2) {
         alert('The bot chose paper. You win! :D')
+        playerScore += 1
     }
     else if (player == 1 && bot == 3) {
         alert('The bot chose scissors. You win!')
+        playerScore += 1
     }
     else if (player == 2 && bot == 3) {
         alert('The bot chose scissors. You lose :(')
+        botScore += 1
     }
     else if (player == 3 && bot == 3) {
         alert('The bot also chose scissors. It\'s a draw!')
     }
 }
-
-compare(playerSelection, botPick)
-
-//Gives buttons event listeners
